d89d489502b8eebf7547089238e52349e6151061
Thiago Assis <thiago.assis@coinext.com.br>
2024-01-18 14:01:32 -0300

    update database version

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-const pool = mysql.createPool({
+export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
